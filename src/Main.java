45dc4de29fd349d3b0e63936483f0fc40352a293
IriS <mhuynk1005@gmail.com>
2022-12-06 14:12:15 +0700

    Add file

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue myQueue = new Queue();
+
+        myQueue.enQueue(3);
+        myQueue.enQueue(2);
+        myQueue.enQueue(1);
+
+
+        myQueue.display();
+
+        myQueue.peak();
+
+
+    }
+}
+
+
